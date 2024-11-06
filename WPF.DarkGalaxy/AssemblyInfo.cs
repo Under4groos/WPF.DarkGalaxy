2870071bba7de872f3effa850d35245df8e7da95
Under4groos <hhit934@gmail.com>
2024-11-07 00:29:08 +0300

    фыв

---
+++ WPF.DarkGalaxy/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+
+[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+                                    
+                                  
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+
+[assembly: XmlnsDefinition("http://github.com/Under4groos/WPF.DarkGalaxy", "WPF.DarkGalaxy.Controls")]
--- WPF.ViewDemo/MainWindow.xaml.cs
+++ WPF.ViewDemo/MainWindow.xaml.cs
@@ -8,13 +8,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF.DarkGalaxy.Controls;
 
 namespace WPF.ViewDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : DarkWindow
     {
         public MainWindow()
         {
